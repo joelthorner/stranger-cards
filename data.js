ec5bda3d85d0d2489fcbee14b20af241dcba3413
Joel Torner <joeltorner@gmail.com>
2017-12-11 23:16:41 +0100

    Card 3 and fixes

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 StrangerCards.dataHttp = window.location.href;
 
 StrangerCards.data = {
+	3 : {
+		name : 'Will before missing',
+		image : StrangerCards.dataHttp + 'images/3.jpg',
+		text : `<b>Mike house</b>
+			Chapter Two: Trick or Treat, Freak (T2)`
+	},
 	11 : {
 		name : 'Eleven',
 		image : StrangerCards.dataHttp + 'images/11.jpg',
-		text : `<b>Hawkins Middle School (upside down)</b>
-			Chapter Two: Trick or Treat, Freak`
+		text : `<b>Fuera de la casa de Mike</b>
+			Chapter One: The Vanishing Of Will Byers (T1)`
 	},
-	12 : {
-		name : 'Eleven',
-		image : StrangerCards.dataHttp + 'images/11.jpg',
-		text : `<b>Hawkins Middle School (upside down)</b>
-			Chapter Two: Trick or Treat, Freak`
-	}
 };
