1ac89b50f116cd2e758c59e3eb14e1c3526bafe9
joelthorner <joeltorner@gmail.com>
2018-06-03 00:07:40 +0200

    2.0

---
+++ js/data.js
@@ -0,0 +1,63 @@
+// TYPES
+// 0 normal
+// 1 glitch
+
+ST.cards = {
+	3 : {
+		name : 'Will before missing',
+		type : 0,
+		image : 'img/3.jpg',
+		location : 'Mike\'s house',
+		episode : 'Chapter Two: Trick or Treat, Freak (S1)'
+	},
+	11 : {
+		name : 'Portal Eleven',
+		type : 1,
+		image : 'img/11.jpg',
+		location : 'Hawkins Middle School (upside down)',
+		episode : 'Chapter Two: Trick or Treat, Freak (S2)'
+	},
+	23 : {
+		name : 'Message wall',
+		type : 0,
+		image : 'img/23.jpg',
+		location : 'Will\'s house',
+		episode : 'Chapter Three: Holly, Jolly (S1)'
+	},
+	26 : {
+		name : 'Jim Hopper without hat',
+		type : 0,
+		image : 'img/26.jpg',
+		location : 'Hawkins police department',
+		episode : 'Chapter Four: The Body (S1)'
+	},
+	32 : {
+		name : 'Flying fucking van',
+		type : 0,
+		image : 'img/32_2.jpg',
+		location : 'Hawkins street',
+		episode : 'Chapter Seven: The Bathtub (S1)'
+	},
+	42 : {
+		name : 'Quarry friends',
+		type : 0,
+		image : 'img/42.jpg',
+		location : 'Hawkins quarry',
+		episode : 'Chapter Six: The Monster (S1)'
+	},
+	50 : {
+		name : 'The Demogorgon',
+		type : 0,
+		image : 'img/50_2.jpg',
+		location : 'Hawkins Middle School',
+		episode : 'Chapter Eight: The Upside Down (S1)'
+	},
+	66 : {
+		name : 'Happy Steisy',
+		type : 0,
+		image : 'img/66.jpg',
+		location : 'Hawkins Middle School',
+		episode : 'Chapter One: The Vanishing Of Will Byers (S1)'
+	}
+
+};
---
+++ js/main.js
@@ -0,0 +1,102 @@
+const DEFAULT_N_CARDS = 1;
+
+ST.manageData = {
+	init : function() {
+		this.initStorage();
+	},
+
+	initStorage : function() {
+
+		// fill emptys or news or not found data
+		localforage.getItem('ST_userCards').then(function(result) {
+			var userCards = {};
+			// demo test
+			result = {
+				11 : 1,
+				3 : 1, 
+				23 : 2, 
+				66 : 20
+			};
+			// end demo test
+
+			if (!result) {
+				$.each(ST.cards, function(num, card) {
+					userCards[num] = DEFAULT_N_CARDS;
+				});
+			}else{
+				$.each(ST.cards, function(num, card) {
+					if (result[num]) {
+						userCards[num] = result[num];
+					}else{
+						userCards[num] = DEFAULT_N_CARDS;
+					}
+				});
+			}
+			// save
+			localforage.setItem('ST_userCards', userCards);
+			// draw
+			ST.layout.fillCards(userCards);
+		})
+	}
+}
+
+ST.layout = {
+	init : function() {
+		// this.fillCards();
+	},
+
+	fillCards : function(userCards) {
+		var $grid = $('.grid');
+
+		$.each(ST.cards, function(num, card) {
+			var num = parseInt(num);
+			var numIncr = num + 1;
+			
+			if (userCards[num]) {
+				$grid.append(
+					`<figure class="card card-ok card-type-${card.type}" data-num="${num}" id="card-${num}" style="-webkit-box-ordinal-group:${numIncr};-ms-flex-order:${num};order:${num}">
+						<div class="inset">
+							<span class="number"><i>${num}</i></span>
+							<div class="bg">
+								<!-- <img src="${card.image}"> -->
+								<div class="bg-img" style="background-image: url(${card.image})"></div>
+								<div class="bg-img" style="background-image: url(${card.image})"></div>
+								<div class="bg-img" style="background-image: url(${card.image})"></div>
+								<div class="bg-img" style="background-image: url(${card.image})"></div>
+								<div class="bg-img" style="background-image: url(${card.image})"></div>
+							</div>
+							<div class="caption">
+								<b class="title" data-text="${card.name}">${card.name}</b>
+								<span class="location">${card.location}</span>
+								<span class="episode">${card.episode}</span>
+							</div>
+						</div>
+					</figure>`
+				);
+			}else{
+				$grid.append(
+					`<figure class="card card-empty" id="card-container-${num}" style="-webkit-box-ordinal-group:${numIncr};-ms-flex-order:${num};order:${num}">
+						<div class="inset">
+							<div class="logo"></div>
+						</div>
+					</figure>`
+				);
+			}
+		});
+		ST.layout.specialTypes();
+	},
+
+	specialTypes : function() {
+		
+		// $('.card-type-1 .bg-img').addClass('glitch');
+	}
+};
+
+ST.main = {
+	init : function () {
+		ST.manageData.init();
+		ST.layout.init();
+	}
+};
+
+$(ST.main.init);
